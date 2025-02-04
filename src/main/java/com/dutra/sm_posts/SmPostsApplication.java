3e3cf320d241b235069a056d4461a0e8c0905b5a
David Alves Dutra <dadutra@hotmail.com>
2025-02-04 17:22:42 -0300

    Adicionado projeto base. SpringInitializer.

---
+++ src/main/java/com/dutra/sm_posts/SmPostsApplication.java
@@ -0,0 +1,13 @@
+package com.dutra.sm_posts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmPostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmPostsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dutra/sm_posts/SmPostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dutra.sm_posts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmPostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
